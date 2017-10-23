9cdd6b8fa27b393e2addbb9a213bedd52cba7d3f
Harsimar Singh <harsimar.singh08@gmail.com>
2017-10-24 01:10:56 +0530

    swap_elements_in_1d_array.
    Write a C++ program to swap first and last element of an integer 1-d array.
    A very good practice for demonstrating the array usage in the language C++.

---
+++ swap_elements_1d_array.cpp
@@ -0,0 +1,31 @@
+//Write a C++ program to swap first and last element of an integer 1-d array.
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int Arr[100],n,i,temp;
+
+	cout<<"Enter number of elements you want to insert ";
+	cin>>n;
+
+	for(i=0;i<n;i++)
+	{
+
+		cout<<"Enter element "<<i+1<<":";
+		cin>>Arr[i];
+	}
+
+	temp=Arr[0];
+	Arr[0]=Arr[n-1];
+
+	Arr[n-1]=temp;
+
+	cout<<"\nArray after swapping"<<endl;
+
+	for(i=0;i<n;i++)
+		cout<<Arr[i]<<" ";
+
+	return 0;
+}
